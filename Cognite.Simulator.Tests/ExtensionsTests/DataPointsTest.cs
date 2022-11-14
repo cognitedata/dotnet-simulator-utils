e3d8c3f6fe0fee63000a83e5a43ba5803c12eef5
Lucas Provensi <lucas.provensi@cognite.com>
2022-11-14 14:28:16 +0100

    Test data point sampling

--- Cognite.Simulator.Tests/ExtensionsTests/DataPointsTest.cs
+++ Cognite.Simulator.Tests/ExtensionsTests/DataPointsTest.cs
@@ -1,4 +1,7 @@
-﻿using Cognite.Simulator.Extensions;
+﻿using Cognite.Extractor.Common;
+using Cognite.Simulator.Extensions;
+using CogniteSdk;
+using Microsoft.Extensions.DependencyInjection;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -40,5 +43,64 @@ namespace Cognite.Simulator.Tests.ExtensionsTests
             Assert.Throws<ArgumentException>(() => DataPointsExtensions.MinutesToGranularity(144001440));
         }
 
+        [Fact]
+        public async Task TestGetSample()
+        {
+            var services = new ServiceCollection();
+            services.AddCogniteTestClient();
+
+            using var provider = services.BuildServiceProvider();
+            var cdf = provider.GetRequiredService<Client>();
+            var dataPoints = cdf.DataPoints;
+
+            // Tests assume the given time series exists in the test project and
+            // that it contains data points in this time range
+            CogniteSdk.TimeRange range = new()
+            {
+                Min = 1632571875265,
+                Max = 1632615015265
+            };
+            
+            // Test max aggregation. A single data point should be returned
+            var (timestamps, values) = await dataPoints.GetSample(
+                "SimConnect-IntegrationTests-OnOffValues",
+                Extensions.DataPointAggregate.Max,
+                800,
+                range,
+                System.Threading.CancellationToken.None).ConfigureAwait(false);
+            Assert.NotNull(timestamps);
+            Assert.NotNull(values);
+            Assert.Single(values);
+            Assert.Equal(1, values[0]);
+
+            CogniteSdk.TimeRange range2 = new()
+            {
+                Min = 0,
+                Max = 0
+            };
+
+            // No data points in the range. Throw exception
+            _ = Assert.ThrowsAsync<DataPointSampleNotFoundException>(
+                async () => await dataPoints.GetSample(
+                  "SimConnect-IntegrationTests-OnOffValues",
+                  Extensions.DataPointAggregate.Max,
+                  1,
+                  range2,
+                  System.Threading.CancellationToken.None)
+                .ConfigureAwait(false));
+
+            // No data points in the range, and before the range. Since aggregation
+            // is step interpolation should search forward in time for any data points
+            var (timestamps2, values2) = await dataPoints.GetSample(
+                "SimConnect-IntegrationTests-OnOffValues",
+                Extensions.DataPointAggregate.StepInterpolation,
+                1,
+                range2,
+                System.Threading.CancellationToken.None).ConfigureAwait(false);
+            Assert.NotNull(timestamps2);
+            Assert.NotNull(values2);
+            Assert.Single(values2);
+            Assert.Equal(1, values2[0]);
+        }
     }
 }
