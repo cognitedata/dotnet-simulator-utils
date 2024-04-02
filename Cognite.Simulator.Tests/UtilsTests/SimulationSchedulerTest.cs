2d28675d519703b9cb8f01875a8bf6ac7eea63a1
abdullah-cognite <100700755+abdullah-cognite@users.noreply.github.com>
2024-04-02 22:36:18 +0200

    update test

--- Cognite.Simulator.Tests/UtilsTests/SimulationSchedulerTest.cs
+++ Cognite.Simulator.Tests/UtilsTests/SimulationSchedulerTest.cs
@@ -136,7 +136,7 @@ namespace Cognite.Simulator.Tests.UtilsTests
                 config,
                 configLib, 
                 logger,
-                config.Simulators,
+                null,
                 cdf)
         {
         }
