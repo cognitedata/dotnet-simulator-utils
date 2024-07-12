e4cd527e2e53286e59544aa16719b9becb703528
abdullah-cognite <100700755+abdullah-cognite@users.noreply.github.com>
2024-07-12 08:56:20 +0200

    update

--- Sample.Calculator.Simulator/Program.cs
+++ Sample.Calculator.Simulator/Program.cs
@@ -23,51 +23,5 @@ public static class SampleConnector {
         DefaultConnectorRuntime<CustomAutomationConfig,CalculatorModelFilestate, CalculatorFileStatePoco>.ConnectorName = "Calculator";
         await DefaultConnectorRuntime<CustomAutomationConfig,CalculatorModelFilestate, CalculatorFileStatePoco>.RunStandalone().ConfigureAwait(false);
     }
-
-    public class CalculatorModelFilestate : ModelStateBase
-    {
-        public CalculatorModelFilestate() : base() { }
-
-        public override CalculatorFileStatePoco GetPoco() {
-            var poco = base.GetPoco();
-            var newObj = new CalculatorFileStatePoco{};
-            FillProperties(poco, newObj);
-            newObj.ModelType = ModelType;
-            return newObj;
-        }
-
-        public override bool ShouldProcess()
-        {
-            // Since the ModelType is an extended value it should be saved into the state.db after the 
-            // first extraction and then read from there. If the value is null at this point this means  
-            // that the last extraction state has been lost due to deletion of the state.db so we need 
-            // to return true to reparse it.
-            if (ModelType == null) {
-                return true;
-            }
-            return base.ShouldProcess();
-        }
-
-        public override void Init(FileStatePoco poco)
-        {
-            base.Init(poco);
-            if (poco is CalculatorFileStatePoco mPoco)
-            {
-                ModelType = mPoco.ModelType ;
-            }
-        }
-
-        public bool Processed {get; set; }
-
-        public override bool IsExtracted => Processed;
-
-        public string ModelType {get; set; }
-    }
-
-    public class CalculatorFileStatePoco : ModelStateBasePoco
-    {
-        [StateStoreProperty("extra-model-type")]
-        public string ModelType { get; internal set; }
-    }
 }
 
--- Sample.Calculator.Simulator/SimulatorAutomation.cs
+++ Sample.Calculator.Simulator/SimulatorAutomation.cs
@@ -1,26 +1,78 @@
+using Cognite.Extractor.StateStorage;
 using Cognite.Simulator.Utils;
 using Cognite.Simulator.Utils.Automation;
 using CogniteSdk.Alpha;
 using Microsoft.Extensions.Logging;
 using static SampleConnector;
 
-public class CalculatorSimulatorAutomationClient : 
-        AutomationClient,
-        ISimulatorClient<CalculatorModelFilestate, SimulatorRoutineRevision> {
+public class CalculatorModelFilestate : ModelStateBase
+{
+    public CalculatorModelFilestate() : base() { }
 
-    
+    public override CalculatorFileStatePoco GetPoco()
+    {
+        var poco = base.GetPoco();
+        var newObj = new CalculatorFileStatePoco { };
+        FillProperties(poco, newObj);
+        newObj.ModelType = ModelType;
+        return newObj;
+    }
+
+    public override bool ShouldProcess()
+    {
+        // Since the ModelType is an extended value it should be saved into the state.db after the 
+        // first extraction and then read from there. If the value is null at this point this means  
+        // that the last extraction state has been lost due to deletion of the state.db so we need 
+        // to return true to reparse it.
+        if (ModelType == null)
+        {
+            return true;
+        }
+        return base.ShouldProcess();
+    }
+
+    public override void Init(FileStatePoco poco)
+    {
+        base.Init(poco);
+        if (poco is CalculatorFileStatePoco mPoco)
+        {
+            ModelType = mPoco.ModelType;
+        }
+    }
+
+    public bool Processed { get; set; }
+
+    public override bool IsExtracted => Processed;
+
+    public string ModelType { get; set; }
+}
+
+public class CalculatorFileStatePoco : ModelStateBasePoco
+{
+    [StateStoreProperty("extra-model-type")]
+    public string ModelType { get; internal set; }
+}
+
+public class CalculatorSimulatorAutomationClient :
+        AutomationClient,
+        ISimulatorClient<CalculatorModelFilestate, SimulatorRoutineRevision>
+{
+
+
     private readonly ILogger<CalculatorSimulatorAutomationClient> _logger;
 
     public CalculatorSimulatorAutomationClient(
-        ILogger<CalculatorSimulatorAutomationClient> logger, 
-        DefaultConfig<CustomAutomationConfig> config): base(logger, config.Automation) {
+        ILogger<CalculatorSimulatorAutomationClient> logger,
+        DefaultConfig<CustomAutomationConfig> config) : base(logger, config.Automation)
+    {
         _logger = logger;
     }
 
     public void ExtractModelInformation(CalculatorModelFilestate state, CancellationToken _token)
     {
         _logger.LogInformation("Begin model information extraction");
-        if (state == null) {
+        if (state == null)
+        {
             throw new Exception("State is not defined");
         }
         state.ModelType = "PARSED";
@@ -41,28 +93,26 @@ public class CalculatorSimulatorAutomationClient :
     }
 
     public Task<Dictionary<string, SimulatorValueItem>> RunSimulation(
-        CalculatorModelFilestate modelState, 
-        SimulatorRoutineRevision routineRevision, 
+        CalculatorModelFilestate modelState,
+        SimulatorRoutineRevision routineRevision,
         Dictionary<string, SimulatorValueItem> inputData
-    ) {
+    )
+    {
+        if (modelState == null) {
+            throw new Exception("Model state is not defined");
+        }
         _logger.LogInformation("CalculatorClient Running a simulation");
         _logger.LogInformation($"Model type : {modelState.ModelType}");
         try
         {
-            Dictionary<string, SimulatorValueItem> result = new Dictionary<string, SimulatorValueItem>();
             var routine = new CalculatorRoutine(routineRevision, inputData);
-            result = routine.PerformSimulation();
-            foreach (var kvp in result)
-            {
-                Console.WriteLine($"Key: {kvp.Key}, Value: {kvp.Value}");
-            }
-
+            var result = routine.PerformSimulation();
             return Task.FromResult(result);
         }
         finally
         {
         }
-        
+
     }
 
     protected override void PreShutdown()
@@ -79,8 +129,8 @@ internal class CalculatorRoutineAutomation : RoutineImplementationBase
 
     public override SimulatorValueItem GetOutput(SimulatorRoutineRevisionOutput outputConfig, Dictionary<string, string> arguments)
     {
-        Console.WriteLine("Handling outputs");
-        var resultItem = new SimulatorValueItem() {
+        var resultItem = new SimulatorValueItem()
+        {
             SimulatorObjectReference = new Dictionary<string, string> {
                 { "objectName", "a" },
                 { "objectProperty", "b" },
