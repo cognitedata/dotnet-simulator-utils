637e157ef4d7b7f322c44248a1ae6bada5a2090c
Ivan Polomani <ivan.polomanyi@cognite.com>
2024-10-01 17:05:42 +0200

    Add retry logic for tests (prevent 429)

--- Cognite.Simulator.Tests/CdfTestClient.cs
+++ Cognite.Simulator.Tests/CdfTestClient.cs
@@ -85,7 +85,10 @@ namespace Cognite.Simulator.Tests
             });
 
             // Configure CDF Client
-            services.AddHttpClient<Client.Builder>();
+            services.AddHttpClient<Client.Builder>()
+                .AddPolicyHandler((provider, message) => {
+                    return CogniteExtensions.GetRetryPolicy(provider.GetService<ILogger<Client>>(), 5, 5000);
+                });
             services.AddSingleton(p => {
                 var auth = p.GetRequiredService<IAuthenticator>();
                 var builder = p.GetRequiredService<Client.Builder>();
