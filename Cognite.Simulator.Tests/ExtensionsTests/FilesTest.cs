28d0de2a2875932b21c97742922b2828d3548bbb
Ivan Polomani <ivano.polomani@gmail.com>
2025-08-05 09:25:00 +0200

    feat(model-library): support original file extension [POFSP-1111] (#281)
    Stacked on top of:
    https://github.com/cognitedata/dotnet-simulator-utils/pull/274 (more
    changes will follow)
    
    Why:
    This is both a logical improvement (paying the tech dept) but also an
    feature that unblocks our current inititive (support model external
    dependencies).
    
    What:
    So far we had been locking file extension to the "first item in the list
    of supported file extesions for a given simulator".
    
    
    This has some major downsides:
    1. Actual file extesion is completely ignored
    3. Not possible to support multiple file extensions per simulator
    (needed soon for the new feature)
    
    Changes:
    1. do not call simulators API, rather take the extension from the name
    of the file (this is something we validate when user uploads the model)

---
+++ Cognite.Simulator.Tests/ExtensionsTests/FilesTest.cs
@@ -0,0 +1,83 @@
+using System;
+
+using Cognite.Simulator.Extensions;
+
+using CogniteSdk;
+
+
+using Xunit;
+
+namespace Cognite.Simulator.Tests.ExtensionsTests
+{
+    public class FilesTests
+    {
+        [Theory]
+        [InlineData("test.txt", "txt")]
+        [InlineData("file.PDF", "pdf")]
+        [InlineData("path/to/file.DOC", "doc")]
+        [InlineData("back\\slash\\file.txt", "txt")]
+        [InlineData("multi.part.name.XML", "xml")]
+        [InlineData("file.with.dots.in.name.json", "json")]
+        [InlineData(".startswithdot", "startswithdot")]
+        public void GetExtension_ValidFileName_ReturnsLowercaseExtension(string fileName, string expectedExtension)
+        {
+            // Arrange
+            var file = new File
+            {
+                Name = fileName,
+                Id = 123
+            };
+
+            // Act
+            var extension = file.GetExtension();
+
+            // Assert
+            Assert.Equal(expectedExtension, extension);
+        }
+
+        [Fact]
+        public void GetExtension_NullFile_ThrowsArgumentNullException()
+        {
+            // Arrange
+            File? file = null;
+            // Act & Assert
+            var exception = Assert.Throws<ArgumentNullException>(() => file.GetExtension());
+            Assert.Equal("File cannot be null. (Parameter 'file')", exception.Message);
+        }
+
+        [Theory]
+        [InlineData("")]
+        [InlineData(null)]
+        public void GetExtension_NullOrEmptyFileName_ThrowsArgumentException(string? fileName)
+        {
+            // Arrange
+            var file = new File
+            {
+                Name = fileName,
+                Id = 123
+            };
+
+            // Act & Assert
+            var exception = Assert.Throws<ArgumentException>(() => file.GetExtension());
+            Assert.Contains("File name cannot be null or empty. File ID: 123", exception.Message);
+        }
+
+        [Theory]
+        [InlineData("noextension")]
+        [InlineData("ends.with.")]
+        [InlineData("   ")]
+        public void GetExtension_InvalidFileName_ThrowsArgumentException(string fileName)
+        {
+            // Arrange
+            var file = new File
+            {
+                Name = fileName,
+                Id = 123
+            };
+
+            // Act & Assert
+            var exception = Assert.Throws<ArgumentException>(() => file.GetExtension());
+            Assert.Contains("File name does not contain a valid extension", exception.Message);
+        }
+    }
+}
