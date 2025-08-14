54b61fe320b9ac3b89125a343b4a16e5d9cc6450
Ivan Polomani <ivan.polomanyi@cognite.com>
2025-08-14 17:23:20 +0200

    feat(modellib): support more than 1000 files per model

--- Cognite.Simulator.Extensions/FilesExtensions.cs
+++ Cognite.Simulator.Extensions/FilesExtensions.cs
@@ -1,5 +1,13 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.IO;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+using Cognite.Extractor.Common;
+
+using CogniteSdk.Resources;
 
 namespace Cognite.Simulator.Extensions;
 
@@ -9,6 +17,50 @@ namespace Cognite.Simulator.Extensions;
 /// </summary>
 public static class FilesExtensions
 {
+    private const int THROTTLE_SIZE = 5;
+    private const int CHUNK_SIZE = 1000;
+
+    /// <summary>
+    /// Retrieves file medatadata by their IDs in chunks, ignores unknown IDs.
+    /// Each batch can contain up to 1000 file IDs and is be processed in parallel (up to 5 batches at a time).
+    /// </summary>
+    /// <param name="cdfFiles">The FilesResource instance.</param>
+    /// <param name="internalIds">The list of internal IDs of the files to retrieve.</param>
+    /// <param name="token">The cancellation token.</param>
+    public static async Task<List<CogniteSdk.File>> RetrieveBatchAsync(
+        this FilesResource cdfFiles,
+        IList<long> internalIds,
+        CancellationToken token = default
+    )
+    {
+        var result = new List<CogniteSdk.File>();
+        object mutex = new object();
+
+        if (internalIds == null || internalIds.Count == 0)
+        {
+            return result;
+        }
+
+        var fileIdsByChunks = internalIds
+            .ChunkBy(CHUNK_SIZE);
+
+        var generators = fileIdsByChunks
+            .Select<IEnumerable<long>, Func<Task>>(
+            (chunk, _) => async () =>
+            {
+                var found = await cdfFiles
+                    .RetrieveAsync(chunk, true, token)
+                    .ConfigureAwait(false);
+                lock (mutex)
+                {
+                    result.AddRange(found);
+                }
+            });
+
+        await generators.RunThrottled(THROTTLE_SIZE, token).ConfigureAwait(false);
+
+        return result;
+    }
 
     /// <summary>
     /// Returns the file extension of a given CDF file.
