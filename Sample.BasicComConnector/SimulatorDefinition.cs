999a9fcbacde4b1c1c894cf862b8a272d8664d71
Everton Colling <evertoncolling@gmail.com>
2025-11-04 23:05:14 +0100

    fix identation

--- Sample.BasicComConnector/SimulatorDefinition.cs
+++ Sample.BasicComConnector/SimulatorDefinition.cs
@@ -16,54 +16,54 @@ static class SimulatorDefinition
                     }
                 },
             StepFields = new List<SimulatorStepField> {
-                    new SimulatorStepField {
-                        StepType = "get/set",
-                        Fields = new List<SimulatorStepFieldParam> {
-                            new SimulatorStepFieldParam {
-                                Name = "sheet",
-                                Label = "Sheet Name",
-                                Info = "Name of the worksheet (e.g., 'Sheet1')",
-                            },
-                            new SimulatorStepFieldParam {
-                                Name = "cell",
-                                Label = "Cell Reference",
-                                Info = "Excel cell reference (e.g., 'A1', 'B2', 'C3')",
-                            },
+                new SimulatorStepField {
+                    StepType = "get/set",
+                    Fields = new List<SimulatorStepFieldParam> {
+                        new SimulatorStepFieldParam {
+                            Name = "sheet",
+                            Label = "Sheet Name",
+                            Info = "Name of the worksheet (e.g., 'Sheet1')",
+                        },
+                        new SimulatorStepFieldParam {
+                            Name = "cell",
+                            Label = "Cell Reference",
+                            Info = "Excel cell reference (e.g., 'A1', 'B2', 'C3')",
                         },
                     },
-                    new SimulatorStepField {
-                        StepType = "command",
-                        Fields = new List<SimulatorStepFieldParam> {
-                            new SimulatorStepFieldParam {
-                                    Name = "command",
-                                    Label = "Command",
-                                    Info = "Select a command",
-                                    Options = new List<SimulatorStepFieldOption> {
-                                        new SimulatorStepFieldOption {
-                                            Label = "Pause Calculations",
-                                            Value = "Pause",
-                                        },
-                                        new SimulatorStepFieldOption {
-                                            Label = "Perform Calculation",
-                                            Value = "Calculate",
-                                        }
-                                    },
+                },
+                new SimulatorStepField {
+                    StepType = "command",
+                    Fields = new List<SimulatorStepFieldParam> {
+                        new SimulatorStepFieldParam {
+                            Name = "command",
+                            Label = "Command",
+                            Info = "Select a command",
+                            Options = new List<SimulatorStepFieldOption> {
+                                new SimulatorStepFieldOption {
+                                    Label = "Pause Calculations",
+                                    Value = "Pause",
                                 },
-                        },
-                    },
-                },
-            UnitQuantities = new List<SimulatorUnitQuantity>() {
-                    new SimulatorUnitQuantity {
-                        Name = "Unitless",
-                        Label = "Unitless",
-                        Units = new List<SimulatorUnitEntry> {
-                            new SimulatorUnitEntry {
-                                Name = "",
-                                Label = "",
+                                new SimulatorStepFieldOption {
+                                    Label = "Perform Calculation",
+                                    Value = "Calculate",
+                                }
                             },
                         },
                     },
                 },
+            },
+            UnitQuantities = new List<SimulatorUnitQuantity>() {
+                new SimulatorUnitQuantity {
+                    Name = "Unitless",
+                    Label = "Unitless",
+                    Units = new List<SimulatorUnitEntry> {
+                        new SimulatorUnitEntry {
+                            Name = "",
+                            Label = "",
+                        },
+                    },
+                },
+            },
         };
     }
 }
